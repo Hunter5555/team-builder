541f842d0c1f49260b90ba316d38023771502d1c
Hunter5555 <hunter.m.allan@gmail.com>
2019-10-17 13:55:24 -0600

    hunter allan, MVP

---
+++ team-builder/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import './App.css';
+import Form from './components/Form';
+import Member from './components/Member';
+
+function App() {
+  const [members, setMembers] = useState([]) 
+
+  return (
+    <div className="App">
+      <Form setMembers={setMembers} members={members}/>
+      {members.map( (member, i ) => <Member member={member} key = {i }/>)}
+      </div>
+  );
+
+}
+export default App;
